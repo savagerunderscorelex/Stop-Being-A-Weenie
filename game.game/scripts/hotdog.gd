7c811e94329eb3db672d032d0926f91dbf0ae5c3
savagerunderscorelex <eigbekoyi@gmail.com>
2025-10-10 20:02:00 -0400

    trying to make the sprite change animation based on inputs (left and right), working on adjusting it since it messes up the scaling of the sprite when inputting

--- game.game/scripts/hotdog.gd
+++ game.game/scripts/hotdog.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
-@export var speed : float = 500 # @export adds a variable to the inspector menu of the parent node (that's actually so cool)
+@export var speed : float = 120 # @export adds a variable to the inspector menu of the parent node (that's actually so cool)
+@onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void: # This function takes a float as an input
 	get_input()
+	update_animation()
 	move_and_slide() # Can only be used in a Character2D Node
 
 func get_input() -> void:
 	var direction = Input.get_axis("move_left", "move_right") # Gets 2 input actions for input, the right side of the assignment operator returns a value between -1 and 1, -1 for left, 1 for right
 	velocity.x = direction * speed
+
+func update_animation() -> void:
+	if velocity.x == 0: # Checks if the player is not moving, since the range for directions is -1 to 1, and 0 is no movement
+		animator.play("idle") # Makes the current animation the idle animation
+		return
+	elif velocity.x < 0:
+		animator.flip_h = -1
+		animator.play("walking_right")
+	elif velocity.x > 0: 
+		animator.flip_h = 1
+		animator.play("walking_right")
